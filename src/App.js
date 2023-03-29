ee20e50600da125c7d734cf462fb5d2d9aa082ac
Daniel Stowe <zellek@live.com>
2023-03-29 14:59:30 -0600

    ip chanbge

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const initialState = {
   box: {}, 
   route: 'signin',
   isSignedIn: false,
-  ip: 'https://10.0.15.224',
+  ip: 'https://75.2.19.189',
   user: {
     id: '',
     name: '',
@@ -34,7 +34,7 @@ class App extends Component {
       box: {}, 
       route: 'signin',
       isSignedIn: false,
-      ip: 'https://10.0.15.224',
+      ip: 'https://75.2.19.189',
       user: {
         id: '',
         name: '',
