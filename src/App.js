ef714f7035f9a0e98200de32d4c985b58cb8e9a0
Daniel Stowe <zellek@live.com>
2023-03-29 15:16:33 -0600

    dynamic ip for server lb

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const initialState = {
   box: {}, 
   route: 'signin',
   isSignedIn: false,
-  ip: 'internal-stowe-code-server-lb-704556116.us-east-1.elb.amazonaws.com',
+  ip: process.env.serverlb,
   user: {
     id: '',
     name: '',
@@ -34,7 +34,7 @@ class App extends Component {
       box: {}, 
       route: 'signin',
       isSignedIn: false,
-      ip: 'internal-stowe-code-server-lb-704556116.us-east-1.elb.amazonaws.com',
+      ip: process.env.serverlb,
       user: {
         id: '',
         name: '',
